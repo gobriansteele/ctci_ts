bd3fe28c2e2b5a4cbe71c3ed7f2cc24a8720da38
Brian Steele <brian.steele@hey.com>
2023-10-09 17:34:01 -0600

    Two more string algos

--- src/chapter1/1.test.ts
+++ src/chapter1/1.test.ts
@@ -1,5 +1,5 @@
 import {test, expect} from "vitest";
-import {isUnique, isPermutation, urlIfy} from "./1.ts";
+import {isUnique, isPermutation, urlIfy, isPalindromePermutation, isOneAway, compressString} from "./1.ts";
 
 test('isUnique', ()=>{
     expect(isUnique('abc')).toBe(true);
@@ -15,4 +15,24 @@ test('isPermutation', ()=>{
 
 test('urlIfy', ()=>{
     expect(urlIfy('Mr John Smith    ')).toBe('Mr%20John%20Smith')
+})
+
+test('isPalindromePermutation', ()=>{
+    expect(isPalindromePermutation('taco cat')).toBe(true);
+    expect(isPalindromePermutation('   taCo cAt')).toBe(true);
+    expect(isPalindromePermutation('brian steele')).toBe(false);
+})
+
+test('isOneAway', ()=>{
+    expect(isOneAway('pale', 'ple')).toBe(true);
+    expect(isOneAway('pales', 'pale')).toBe(true);
+    expect(isOneAway('pale', 'bale')).toBe(true);
+    expect(isOneAway('pale', 'bake')).toBe(false);
+})
+
+test('compressString', ()=>{
+    expect(compressString('aabcccccaaa')).toBe('a2b1c5a3');
+    expect(compressString('abc')).toBe('abc');
+    expect(compressString('aaabbbccc')).toBe('a3b3c3');
+
 })
--- src/chapter1/1.ts
+++ src/chapter1/1.ts
@@ -51,3 +51,105 @@ export function urlIfy(str: string): string {
     return newStr.join('').trim()
 
 }
+
+export function isPalindromePermutation(str: string): boolean {
+    const charMap = new Map<string, number>()
+    let charCount = 0
+
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] !== ' ') {
+            charCount++
+            charMap.set(str[i].toLowerCase(), (charMap.get(str[i].toLowerCase()) || 0) + 1)
+        }
+    }
+    if(charCount %2 ===0) {
+        for(let [_key, value] of charMap) {
+            if(value %2 !== 0) {
+                return false
+            }
+        }
+    } else {
+        let oddCount = 0
+        for(let [_key, value] of charMap) {
+            if(value %2 !== 0) {
+                oddCount++
+            }
+        }
+        if(oddCount !== 1) {
+            return false
+        }
+    }
+    return true
+
+}
+
+export function isOneAway(strA: string, strB: string): boolean {
+    const lengthDifference = Math.abs(strA.length - strB.length)
+    const shorterString = strA.length <= strB.length ? strA : strB
+    const longerString = strA.length > strB.length ? strA : strB
+
+    if(lengthDifference > 1) {
+        return false
+    }
+    if(lengthDifference === 1) {
+        // all characters in shorter string must be in longer string
+        const charMap = new Map<string, boolean>()
+        for(let i = 0; i < longerString.length; i++) {
+            charMap.set(longerString[i], true)
+        }
+        for(let i = 0; i < shorterString.length; i++) {
+            if(!charMap.get(shorterString[i])) {
+                return false
+            }
+        }
+        return true
+
+    } else {
+        // all characters in both strings must be the same except for one
+        const charArray = new Array(128).fill(0)
+        let diffCount = 0
+        for(let i = 0; i < strA.length; i++) {
+            const charCode = strA.charCodeAt(i)
+            charArray[charCode]++
+        }
+        for(let i = 0; i < strB.length; i++) {
+            const charCode = strB.charCodeAt(i)
+            charArray[charCode]--
+            if(charArray[charCode] < 0) {
+                diffCount++
+            }
+            if(diffCount > 1) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+export function compressString(str: string): string {
+    let compressedString = ''
+    let currentChar = str[0]
+    let currentCharCount = 1
+    let didCompress = false
+
+
+    for(let i = 0; i < str.length; i++) {
+        const nextChar = str[i + 1]
+        if(nextChar === currentChar) {
+            currentCharCount++
+            didCompress = true
+        }
+
+        if(nextChar !== currentChar || i === str.length -1) {
+            compressedString = `${compressedString}${currentChar}${currentCharCount}`
+            currentChar = nextChar
+            currentCharCount = 1
+        }
+    }
+
+        if(!didCompress) {
+            return str
+        }
+        return compressedString
+
+}
